83c40508af0cf0d1e7590b7619cc6f224b549422
muhammadrayaarrizki <muhammadrayaarrizki@gmail.com>
2026-01-11 20:18:26 +0700

    feat: change biome to oxlint, adding pre-commit & adding checkbuild on pr

--- app/app.vue
+++ app/app.vue
@@ -1,53 +1,72 @@
 <script setup lang="ts">
-import axios from 'axios';
-import { reactive } from 'vue';
+import axios from "axios";
+import { reactive } from "vue";
 
+const { user, isLoggedIn, isLoading, fetchSession } = useAuth();
+
+onMounted(() => {
+	fetchSession();
+});
 const credentials = reactive({
-  email:"",
-  password:""
-})
+	email: "",
+	password: "",
+});
 
 const itemTitle = reactive({
-  title:""
-})
+	title: "",
+});
 
-const createItem = async() =>{
-  try {
-    const response = await axios.post("http://localhost:9999/api/items", itemTitle, {
-      withCredentials: true // This sends cookies for session auth
-    })
-    console.log(response.data)
-  } catch (error) {
-    console.log(error)
-  }
-} 
+const createItem = async () => {
+	try {
+		const response = await axios.post(
+			"http://localhost:9999/api/items",
+			itemTitle,
+			{
+				withCredentials: true, // This sends cookies for session auth
+			},
+		);
+		console.log(response.data);
+	} catch (error) {
+		console.log(error);
+	}
+};
 
-async function login(){
-  try {
-    const response = await axios.post("http://localhost:9999/api/auth/sign-in/email", credentials, {
-      withCredentials: true // This receives and stores the session cookie
-    })
-    console.log(response.data)
-  } catch (error) {
-    console.log(error)
-  }
+async function login() {
+	try {
+		const response = await axios.post(
+			"http://localhost:9999/api/auth/sign-in/email",
+			credentials,
+			{
+				withCredentials: true, // This receives and stores the session cookie
+			},
+		);
+		console.log(response.data);
+	} catch (error) {
+		console.log(error);
+	}
 }
 </script>
 
 <template>
-  <div>
-    <div>login</div>
-    <form @submit.prevent="login" action="post">
-      <input v-model="credentials.email" type="text" name="email">
-      <input v-model="credentials.password" type="text" name="password">
-      <button type="submit">login</button>
-    </form>
+  <div v-if="isLoggedIn">
+    <div>
+      Welcome, {{ user.name }}  {{ user.email }}
+      user id: {{ user.id }}
+      <div>create item</div>
+      <form @submit.prevent="createItem" action="post">
+        <input v-model="itemTitle.title" type="text" name="title">
+        <button type="submit">create</button>
+      </form>
+    </div>
   </div>
-  <div>
-    <div>create item</div>
-    <form @submit.prevent="createItem" action="post">
-      <input v-model="itemTitle.title" type="text" name="title">
-      <button type="submit">create</button>
-    </form>
+  <div v-else>
+ <div>
+      <div>login</div>
+      <form @submit.prevent="login" action="post">
+        <input v-model="credentials.email" type="text" name="email">
+        <input v-model="credentials.password" type="text" name="password">
+        <button type="submit">login</button>
+      </form>
+    </div>
   </div>
 </template>
